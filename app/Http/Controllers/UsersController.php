7fc501d89521d7b48469be2001cb8ea78e7b5939
Gašper <gasper.irman@gmail.com>
2018-10-18 14:32:01 +0200

    Initial commit

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use DB;
+use Illuminate\Support\Facades\Storage;
+
+class UsersController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index($id)
+    {
+    	$user = User::find($id);
+        return view('account.index')->with('user', $user);
+    }
+
+    public function update(Request $request, $id)
+    {
+    	        $this ->validate($request, [
+        		'email' => 'email',
+        		'profile_picture' => 'image|nullable|max:1999'
+        ]);
+        $user = User::find($id);
+
+        //Handle file upload
+        if($request->hasFile('profile_picture')){
+            //Get filename with extenstion
+            $filenameWithExt = $request->file('profile_picture')->getClientOriginalName();
+            //Get just filename
+            $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+            //Get just extension
+            $ext = $request->file('profile_picture')->getClientOriginalExtension();
+            //Filename to store
+            $fileNameToStore = $filename.'_'.time().'.'.$ext;
+            //Upload
+            $path = $request->file('profile_picture')->storeAs('public/profile_pictures', $fileNameToStore);
+        }
+        else{
+        	$fileNameToStore = 'wat zka ni slike';
+        }
+        $user->name = $request->input('name');
+        if($request->hasFile('profile_picture'))
+        {
+            $user->profile_picture = $fileNameToStore;
+        }
+        $user->email = $request->input('email');
+        $user->save();
+        return redirect('users/'.$user->id)->with('success', 'User information updated successfully.');
+    }
+}
---
+++ index.php
@@ -0,0 +1,1 @@
+https://mail.google.com/mail/u/0/?view=cm&fs=1&to=gasper.irman@gmail.com&su=Contact&tf=1
